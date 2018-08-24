a890380ad8a5b2ecf49d7d6a962d19b51754844e
Andrew Hill <mdude2314@gmail.com>
2018-08-24 11:40:42 -0400

    fix scroll, tweak hero styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,23 +7,24 @@ import { NetlifyService } from './services/netlify.service';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-
   name: string;
   email: string;
   isInterestedInSpeaking = false;
-  constructor(private netlifyService: NetlifyService) {
-
-  }
+  constructor(private netlifyService: NetlifyService) {}
   scrollToSignUp = () => {
     const el = document.querySelector('#sign-up') as HTMLElement;
-    el.scrollIntoView({ behavior: 'smooth' });
-  }
+    el.scrollIntoView({ behavior: 'smooth', block: 'start' });
+  };
 
   onFormSubmit = () => {
-    this.netlifyService.postForm(this.name, this.email, this.isInterestedInSpeaking);
+    this.netlifyService.postForm(
+      this.name,
+      this.email,
+      this.isInterestedInSpeaking
+    );
     this.name = null;
     this.email = null;
     this.isInterestedInSpeaking = false;
     return false;
-  }
+  };
 }
